84cf75eb9f67ea14118e3080887100e81534a55d
Vlad Ilie Filip <vladiliefilip@icloud.com>
2023-02-12 13:12:34 +0200

    Initial Commit

---
+++ RetirementCalculator/RetirementCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RetirementCalculatorApp.swift
+//  RetirementCalculator
+//
+//  Created by Vlad Filip on 12.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct RetirementCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
